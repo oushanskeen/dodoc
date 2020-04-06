0d26dd83c79c6c2f3de68c5f8c496a3859a33bba
Oushan Skeen <inta.soul@gmail.com>
2020-04-06 19:38:07 +0300

    DODOC-New-Home

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+
+function App() {
+  return (
+    <main>
+        <Switch>
+            <Route exact path="/dodoc/" 
+                component={Home} />          
+        </Switch>
+    </main>
+  );
+}
+
+export default App;
