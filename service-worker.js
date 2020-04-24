f2ef5d94c333bfb21c1d5078cf9880b97454dc52
Oushan Skeen <inta.soul@gmail.com>
2020-04-24 04:59:32 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dodoc/precache-manifest.73bdd934fe42ffcc30c1140efe42ad89.js"
+  "/dodoc/precache-manifest.6fe29a596925b0367f09070fcfce013c.js"
 );
 
 self.addEventListener('message', (event) => {
