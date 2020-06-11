4fc70c2d3d52fb909dc826bbefeaf4e1c255a7f4
Oushan Skeen <inta.soul@gmail.com>
2020-06-11 06:46:43 +0300

    One more processed route, hope it will help to let the site work

--- src/App.js
+++ src/App.js
@@ -69,7 +69,9 @@ function App() {
               <Route path="/dodoc/ownerdic">
                 <Ownerdic />
               </Route>
-              <Route path="/dodoc/agentdic" component={Agentdic} />
+              <Route path="/dodoc/agentdic">
+                <Agentdic />
+              </Route>
               <Route exact path="/dodoc/dogdic" component={Dogovordic} />
               <Route exact path="/dodoc/objdic" component={Objectdic} />
               <Route exact path="/dodoc/dogdic/:dogovor" component={Dogovor} />
