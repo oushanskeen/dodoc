86d815c239fb6fd1159b400f0daf7c6bbc5e0779
Oushan Skeen <inta.soul@gmail.com>
2020-07-24 02:13:22 +0300

    Updates

---
+++ precache-manifest.54e0a84d4edea91072b5b3b5f6fe662f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96322099ef55213628538bb568ad78a4",
+    "url": "/dodoc/index.html"
+  },
+  {
+    "revision": "9768478fc1f2f6ef5b57",
+    "url": "/dodoc/static/js/2.cf3626c1.chunk.js"
+  },
+  {
+    "revision": "a0fe6f3e6da980958fc07d8464d7dc2b",
+    "url": "/dodoc/static/js/2.cf3626c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3efb5fa4a67a88ac2b",
+    "url": "/dodoc/static/js/main.70eb1e8e.chunk.js"
+  },
+  {
+    "revision": "d6eafb8e13bf3535d8d7",
+    "url": "/dodoc/static/js/runtime-main.5f7268fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dodoc/precache-manifest.7c051d374efc4117cb6cd31cd412c8f8.js"
+  "/dodoc/precache-manifest.54e0a84d4edea91072b5b3b5f6fe662f.js"
 );
 
 self.addEventListener('message', (event) => {
