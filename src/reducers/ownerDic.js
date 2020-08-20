5ecfa0846bc139178e885f286c7a7f9e5cece50d
Oushan Skeen <inta.soul@gmail.com>
2020-08-20 11:31:45 +0300

    on PUT_ACCESS request change field NAME to field ID

--- src/reducers/ownerDic.js
+++ src/reducers/ownerDic.js
@@ -146,7 +146,7 @@ export default function(state = initialState,action){
     case PUT_OWNER_SUCCESS:
       return {
         data: state.data.map(e => 
-          e.name===action.payload.name 
+          e.id ===action.payload.id 
           ? {...trimMongoReturn(action.payload)} : 
           e
         ),
