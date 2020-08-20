e6dcb63789796e50bc4e064cbc629ca56633856c
Oushan Skeen <inta.soul@gmail.com>
2020-08-21 02:36:57 +0300

    Wab ill useState hook

--- src/components/ELEMENTS/Elements.js
+++ src/components/ELEMENTS/Elements.js
@@ -137,7 +137,7 @@ export const ShowHideButton = ({
 const DetailsButton = async ({ owner, state }) => {
   //const [properOwner, setProperOwner] =  {...owner};
   console.log(`OWNER OF A DETAILS BUTTON : `, owner);
-  {console.log(`STATE IN A DETAILS BUTTON: `, state);
+  console.log(`STATE IN A DETAILS BUTTON: `, state);
   const [properOwner, setProperOwner] = useState({...owner});
   //const responseState = await state;
   //const updatedOwner = 
