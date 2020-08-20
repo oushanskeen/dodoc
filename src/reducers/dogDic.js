7703e74c2f7ffb9b259d099db938bbe475ba365e
Oushan Skeen <inta.soul@gmail.com>
2020-08-21 01:07:11 +0300

    May be subscribe will help

--- src/reducers/dogDic.js
+++ src/reducers/dogDic.js
@@ -129,7 +129,8 @@ export default function(state = initialState, action) {
         error: null
       };
       console.log("out in dogovor reduceri : ", out);
-      console.log("store in dogovor reducer : ", store.getState());
+      //console.log("store in dogovor reducer : ", store.getState());
+      store.subscribe(() => console.info(store.getState()))
       return out;
     case POST_DOGOVOR_SUCCESS:
       return {
